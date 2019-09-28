9b5d3811770138ea7e0b16f47165b82bc267cecf
keke0123 <keke0123@naver.com>
2019-09-28 12:27:21 +0900

    알고리즘 / 프로그래머스 테스트

---
+++ manual/algorithm/programmers/node-test-01.js
@@ -0,0 +1,11 @@
+
+// powershell 에서 node .\node-test-01.js 치면 됨
+
+// node input 관련 예제 코드 / "5 3" 입력하면 5/n3 출력
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', data => {
+//     const n = data.split(" ");
+//     const a = Number(n[0]), b = Number(n[1]);
+//     console.log(a);
+//     console.log(b);
+// });
